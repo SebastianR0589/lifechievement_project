ca6cddaef6d557f5c227cee266b037840fec5e11
SebastianR0589 <sebastian.riedel0589@gmail.com>
2026-02-09 16:17:23 +0100

    backend starter created

---
+++ backend/src/test/java/com/sebastianriedel/backend/LifechievementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianriedel.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifechievementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
