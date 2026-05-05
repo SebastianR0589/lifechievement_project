c21eee15eade460f6a3cb63d0b1f09680ce706e9
SebastianR0589 <sebastian.riedel0589@gmail.com>
2026-05-05 11:59:46 +0200

    Update LifechievementApplicationTests.java

--- backend/src/test/java/com/sebastianriedel/lifechievement/LifechievementApplicationTests.java
+++ backend/src/test/java/com/sebastianriedel/lifechievement/LifechievementApplicationTests.java
@@ -3,13 +3,19 @@ package com.sebastianriedel.lifechievement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.autoconfigure.exclude=org.springframework.boot.testcontainers.service.connection.ServiceConnectionAutoConfiguration"
+})
 class LifechievementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() { }
 }
